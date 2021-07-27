f04f0fb8b6a89e47d8c09f2effa3c159646b1df3
cado-car <arq.carolinadaniel@gmail.com>
2021-07-26 22:24:46 -0300

    Part2

---
+++ ft_strtrim.c
@@ -0,0 +1,27 @@
+/*
+*	DESCRIPTION
+*	Allocates (with malloc(3)) and returns a copy of ’s1’ with the characters 
+*	specified in ’set’ removed from the beginning and the end of the string.
+*	RETURN VALUES
+*	The trimmed string. NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !set)
+		return (NULL);
+	i = 0;
+	while (ft_strchr(set, s1[i]) && s1[i] != '\0')
+		i++;
+	j = ft_strlen((char *)s1);
+	while (ft_strchr(set, s1[j]) && j != 0)
+		j--;
+	if ((int)(j - i + 1) <= 0)
+		return (ft_calloc(1, 1));
+	return (ft_substr(s1, i, (j - i + 1)));
+}
