20e43e284d6062b74238cacba8c855624a07d648
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-07-31 21:48:34 -0300

    Part2

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 11:46:04 by cado-car          #+#    #+#             */
-/*   Updated: 2021/07/31 11:53:03 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/07/31 21:46:31 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,24 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*nbr;
-
-	nbr = ft_itoa(n);
-	ft_putstr_fd(nbr, fd);
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(214748364, fd);
+		ft_putchar_fd('8', fd);
+	}
+	else if (n >= 0 && n < 10)
+	{
+		ft_putchar_fd((n + '0'), fd);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr_fd((n / 10), fd);
+		ft_putchar_fd((n % 10 + '0'), fd);
+	}
+	else
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd((n * -1), fd);
+	}
 }
