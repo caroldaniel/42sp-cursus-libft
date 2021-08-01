21aabbb8f2e81665f9597e43bb91f2db310625f9
Carolina Do carmo daniel <cado-car@c1r5p2.42sp.org.br>
2021-08-01 19:21:30 -0300

    Part2

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 11:40:26 by cado-car          #+#    #+#             */
-/*   Updated: 2021/07/31 23:35:59 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/01 18:52:45 by cado-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (s == NULL)
 		return (NULL);
 	strlen = ft_strlen((char *)s);
-	if (start > strlen)
+	if (start >= strlen)
 		return (ft_strdup(""));
 	sstr = (char *)malloc((len + 1) * sizeof(char));
 	if (sstr == NULL)
 		return (NULL);
-	ft_strlcpy(sstr, (s + start), (len + 1));
+	ft_strlcpy(sstr, &s[start], len + 1);
 	return (sstr);
 }
