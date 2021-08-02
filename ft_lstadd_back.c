d8871d5428781fd755fdcf677fa900ecb5c32a60
Carolina Do carmo daniel <cado-car@c1r2p1.42sp.org.br>
2021-08-02 11:15:10 -0300

    Bonus

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/31 12:10:21 by cado-car          #+#    #+#             */
-/*   Updated: 2021/07/31 12:12:27 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/02 10:25:38 by cado-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*elem;
 	t_list	*temp;
 
-	temp = *lst;
+	if (new == NULL)
+		return ;
 	if (*lst == NULL)
 		*lst = new;
 	else
 	{
-		elem = new;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = elem;
-		elem->next = NULL;
+		temp = ft_lstlast(*lst);
+		temp->next = new;
 	}
 }
